bb48d2e181f088b6d45095a2b9908e9f431f6ea8
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-11-09 12:07:34 +0600

    Add animation in budgeting tips cards

--- src/components/BudgetingTips/BudgetingTips.jsx
+++ src/components/BudgetingTips/BudgetingTips.jsx
@@ -1,4 +1,5 @@
 import { Dot } from 'lucide-react'
+import { motion } from 'motion/react'
 import React from 'react'
 
 const BudgetingTips = () => {
@@ -8,44 +9,44 @@ const BudgetingTips = () => {
                 <h1 className="font-bold text-2xl px-4 my-5">Budgeting Tips</h1>
             </div>
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">💰 1. Follow the 50/30/20 Rule</h1>
                     <ul className="space-y-2 px-4">
                         <li className="flex gap"><Dot />50% Needs: food, rent, transportation, bills</li>
                         <li className="flex gap"><Dot />30% Wants: outings, entertainment, shopping</li>
                         <li className="flex gap"><Dot />20% Savings: emergency fund, investment, future goals</li>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">📒 2. Track Every Expense</h1>
                     <ul className="space-y-2">
                         <li className="flex gap"><Dot />Use tools like Notion, Google Sheets, or budgeting apps (like Wallet, Money Manager, or Spendee).</li>
                         <li className="flex gap"><Dot />Even small daily spends (like snacks or rides) add up fast — record them.</li>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">🎯 3. Set Financial Goals</h1>
                     <ul className="space-y-2">
                         <li className="flex gap"><Dot />Short-term: Buy a new keyboard</li>
                         <li className="flex gap"><Dot />Mid-term: Build a PC upgrade fund</li>
                         <li className="flex gap"><Dot />Long-term: Emergency or investment savings</li>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">💳 4. Avoid Impulse Purchases</h1>
                     <ul className="space-y-2">
                         <h1>Ask yourself:</h1>
                         <p>“Do I need this or just want this right now?”</p>
                         <p className="border-l-3 border-gray-50 pl-2"> If it's a want, wait 48 hours before buying. Most times, you'll skip it.</p>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">🧾 5. Automate Savings</h1>
                     <ul className="space-y-2">
                         <p>If possible, transfer a fixed amount to a savings account (or bKash savings) right after you get money.  <br/>  Treat savings like a “bill” that must be paid.</p>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">🧠 6. Analyze & Adjust Monthly</h1>
                     <ul className="space-y-2">
                         <h1>At the end of each month:</h1>
@@ -53,35 +54,20 @@ const BudgetingTips = () => {
                         <li className="flex gap"><Dot />Adjust next month's limits</li>
                         <li className="flex gap"><Dot />Identify habits to cut or improve</li>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">🛍️ 7. Use Cash or Prepaid Cards for Control</h1>
                     <ul className="space-y-2">
                         <li className="flex gap"><Dot />Paying in cash makes you more aware of spending.</li>
                         <li className="flex gap"><Dot />For online purchases, use a limited prepaid card balance — it prevents overspending.</li>
                     </ul>
-                </div>
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
+                </motion.div>
+                <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }} className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
                     <h1 className="py-4 font-bold text-xl">⚡ 8. Build an Emergency Fund</h1>
                     <ul className="space-y-2">
                         <p className="px-4">Try to save at least 1-2 months of expenses for unexpected situations (medical, tech issues, etc.).</p>
                     </ul>
-                </div>
-
-
-
-                <div className="border border-gray-100 shadow-xl m-4 p-4 rounded-xl bg-gray">
-                    <h1 className="py-4 font-bold text-xl"></h1>
-                    <ul className="space-y-2">
-                        <li className="flex gap"><Dot /></li>
-                        <li className="flex gap"><Dot /></li>
-                        <li className="flex gap"><Dot /></li>
-                    </ul>
-                </div>
-
-
-
-
+                </motion.div>
             </div>
         </div>
     )
