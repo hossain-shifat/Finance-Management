4f340113a7e33e75edbf4d1d1a36ab7e8c9c32cf
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-11-09 11:03:50 +0600

    Add footer

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 import Home from '../pages/Home/Home'
 import { Outlet } from 'react-router'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 const Root = () => {
     return (
         <div>
-           <Navbar/>
-           <Outlet/>
+            <Navbar />
+            <div className="min-h-screen">
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     )
 }
