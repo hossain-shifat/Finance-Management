d5e5272617863eb8935a0a098abb4d5f8b1187d8
Hossain-Shifat <161940464+hossain-shifat@users.noreply.github.com>
2025-11-13 16:46:56 +0600

    Add banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import bannerImg from '../../assets/bannerImg.png'
+import { ArrowRight } from 'lucide-react'
 
 const Banner = () => {
     return (
-        <div className="bg-linear-to-r from-[#0179FE] to-[#4893FF] p-5 m-4 text-center rounded-xl text-white">
-            <p>
-                <span className="font-bold">"Master Your Money, Master Your Future."</span> <br className="hidden md:block"/>
-                <span className="italic">Empower yourself with smart financial management.</span>
-            </p>
+        <div className="flex flex-col-reverse md:flex-row justify-between gap-4 bg-base-200 p-10 text-center md:m-4 md:rounded-2xl text-white">
+            <div className="flex flex-col justify-center items-center gap-2 md:pl-5">
+                <h1 className="font-bold text-base-content text-2xl">Consistency builds your path to success.</h1>
+                <p className="text-base-content max-w-[450px]">Every small action today builds tomorrow. Consistency turns tiny steps into achievements. Push yourself now; your efforts create opportunities.</p>
+                <button className="btn btn-primary ">Get Started <ArrowRight size={18} /></button>
+            </div>
+            <div className="">
+                <img src={bannerImg} alt="" />
+            </div>
         </div>
     )
 }
